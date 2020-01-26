2cfa9277e8955c088ad60c7107ea22a9d0715da8
evgeniitsvetkov <evgeniitsvetkov@yandex.ru>
2020-01-26 21:37:41 +0300

    Save booking into BD from route booking

--- app.py
+++ app.py
@@ -10,7 +10,6 @@ from forms import MessageForm, BookingForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'you-will-never-guess'
-#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dfdlfhqaqadkgd:2a03bd35a434942bf87161053906403340e2187b9f139b1a8ec3ad6eddebc361@ec2-46-137-188-105.eu-west-1.compute.amazonaws.com:5432/d3o5u8br75i2g7'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
@@ -169,7 +168,9 @@ def booking():
     form = BookingForm()
 
     if form.validate_on_submit():
-        new_booking = Booking(day=day, hour=hour, client_name='X', client_phone='Y', teacher=teacher)
+        name = form.name.data
+        phone = form.phone.data
+        new_booking = Booking(day=day, hour=hour, client_name=name, client_phone=phone, teacher=teacher)
         db.session.add(new_booking)
         db.session.commit()
 
