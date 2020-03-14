4911e3930af5b80dc3113003fbc9cb56518b5946
evgeniitsvetkov <evgeniitsvetkov@yandex.ru>
2020-03-14 12:15:04 +0300

    Add config.py

--- app.py
+++ app.py
@@ -7,11 +7,11 @@ from flask_migrate import Migrate
 from werkzeug.utils import redirect
 from forms import MessageForm, BookingForm
 
+from config import Config
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'you-will-never-guess'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://dfdlfhqaqadkgd:2a03bd35a434942bf87161053906403340e2187b9f139b1a8ec3ad6eddebc361@ec2-46-137-188-105.eu-west-1.compute.amazonaws.com:5432/d3o5u8br75i2g7'
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config.from_object(Config)
+
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 
---
+++ config.py
@@ -0,0 +1,4 @@
+class Config:
+    SECRET_KEY = 'you-will-never-guess'
+    SQLALCHEMY_DATABASE_URI = 'postgres://dfdlfhqaqadkgd:2a03bd35a434942bf87161053906403340e2187b9f139b1a8ec3ad6eddebc361@ec2-46-137-188-105.eu-west-1.compute.amazonaws.com:5432/d3o5u8br75i2g7'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
